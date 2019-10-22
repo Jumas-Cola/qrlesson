d9aeb1bc12e8bdee4f9bfcafd1799769740de3d3
starlightx <kbbyfl91@gmail.com>
2019-10-22 22:40:48 +0300

    change admin panel

--- qrmethods/admin.py
+++ qrmethods/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import TeachingMethod, LessonPart
 
 
-admin.site.register(TeachingMethod)
+@admin.register(TeachingMethod)
+class TeachingMethodAdmin(admin.ModelAdmin):
+    list_display = ('title', 'lesson_part', 'author', 'id')
+    list_filter = ('lesson_part', 'author')
+
+
+# admin.site.register(TeachingMethod)
 admin.site.register(LessonPart)
